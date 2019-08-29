7a5ea1f4c72bd90c9adc78bf5fcf790f178419e1
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-29 04:40:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,8 @@
-def map(source_array)
+def map(array)
   new = []
   count = 0 
   while count < source_array.length
-    new.push(yield(source_array[i]))
+    new.push(yield(source_array[count]))
     count+=1 
   end
   new
